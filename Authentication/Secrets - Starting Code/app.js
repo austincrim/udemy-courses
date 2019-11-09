d4acc5773f45d5fca6b53688cc80ca4eeee12bce
Austin <aust.crim@gmail.com>
2019-11-08 22:11:56 -0600

    authentication folder added

--- Authentication/Secrets - Starting Code/app.js
+++ Authentication/Secrets - Starting Code/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -12,14 +12,13 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true, useUnifiedTopology: true});
 
 const userSchema = new mongoose.Schema ({
     email: String,
     password: String
 });
 
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -47,7 +46,7 @@ app.post('/register', function(req, res) {
     
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err) {
         if(err) {
@@ -59,12 +58,14 @@ app.post('/register', function(req, res) {
 });
 
 app.post('/login', function(req, res) {
-    
-    User.findOne({email: req.body.username, password: password}, function(err, user) {
-        if(err) {
-            console.log(err);
-        } else {
+    const username = req.body.username;
+    const password = md5(req.body.password);
+
+    User.findOne({email: req.body.username}, function(err, user) {
+        if(password == user.password) {
             res.render('secrets');
+        } else {
+            res.redirect('/login');
         }
     });
 });
