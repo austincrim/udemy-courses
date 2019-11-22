ee953c605a8c3ead02e5f16ec97d1b9e9ad66fed
Austin <aust.crim@gmail.com>
2019-11-21 19:57:20 -0600

    passing variables to jinja template

--- Flask/app/views.py
+++ Flask/app/views.py
@@ -4,7 +4,7 @@ from app import app
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", name="Python")
 
 @app.route('/about')
 def about():
