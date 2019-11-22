c5fcb26d2faa0ec051539d4836a85b4e1ae62f3d
Austin <aust.crim@gmail.com>
2019-11-21 22:18:00 -0600

    connect to mongo, render posts

--- Python/Flask/app/views.py
+++ Python/Flask/app/views.py
@@ -1,18 +1,38 @@
 from flask import render_template, request
-
 from app import app
+from bson.objectid import ObjectId
+import pymongo
+
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client.blogDB
+posts_clctn = db.posts
+
+# post = {
+#     "title": "Test",
+#     "content": "Test content"
+# }
+
+# post_id = posts.insert_one(post).inserted_id
 
 
-@app.route('/', methods=["GET", "POST"])
+@app.route('/')
 def index():
-    if request.method == 'GET':
-        return render_template("index.html", data="Python")
-
-    if request.method == 'POST':
-        data = request.form['name']
-        return render_template("index.html", data=data)
+    all_posts = posts_clctn.find()
+    return render_template("index.html", posts = all_posts)
 
 
-@app.route('/about')
-def about():
-    return render_template("about.html")
+@app.route('/posts/<post_id>')
+def posts(post_id):
+    print(post_id)
+    postSelected = posts_clctn.find_one({"_id": ObjectId(post_id)}) 
+    print(postSelected)   
+    return render_template("post.html", postSelected = postSelected)
+
+
+@app.route('/compose', methods=["GET", "POST"])
+def compose():
+    if request.method == "GET":
+        return render_template("compose.html")
+
+    # if request.method == "POST":
+    #     TODO
