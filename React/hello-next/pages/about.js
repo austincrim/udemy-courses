3e6a8d4b995ddcd8448680c1d1985a405cf265ce
Austin <aust.crim@gmail.com>
2019-12-20 18:51:07 -0600

    finished next.js basic tutorial

---
+++ React/hello-next/pages/about.js
@@ -0,0 +1,11 @@
+import Layout from "../components/MyLayout";
+
+const About = () => (
+    <div>
+        <Layout>
+            <p>This is the about page</p>
+        </Layout>
+    </div>
+);
+
+export default About;
